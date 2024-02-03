cccacac17e5e3c08a8eaa3366aff6dac98d81059
Brettler <liad.brettler@gmail.com>
2024-02-03 08:43:27 +0200

    background and buttons for main page

---
+++ src/HomePage/welcomeContainer/WelcomeContainer.js
@@ -0,0 +1,21 @@
+import MenuButtons from '../menuButtons/MenuButtons'
+import './WelcomeContainer.css'
+
+function WelcomeContainer() {
+
+
+    return (
+        <div>
+            <MenuButtons/>
+            <div className='profilePic'>
+
+
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default WelcomeContainer;
