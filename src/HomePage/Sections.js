e69a69ff274ff062e8eb0981ccd4be4e184e4e72
Brettler <liad.brettler@gmail.com>
2024-02-06 18:50:27 +0200

    Added animation to the menu, also modify the current animation of the sections

--- src/HomePage/Sections.js
+++ src/HomePage/Sections.js
@@ -1,24 +1,60 @@
 
-import './Sections.css'
+import './Sections.css';
+import MenuButtons from './menuButtons/MenuButtons';
 import FirstSection from './firstSection/FirstSection';
-import MenuButtons from './menuButtons/MenuButtons'
-
-
+import SecondSection from './secondSection/SecondSection';
+import { useEffect, useRef, useState } from 'react';
 
 function Sections() {
 
+    const ref = useRef(null);
+    const [menuOpacity, setMenuOpacity] = useState(1);
+    
 
 
+
+
+    useEffect( () => {
+        const handleMenuPosition = (() => {
+
+            const scrollThreshPos = window.innerHeight * 0.2; // Define the hight in which when the user scroll under we set fixed to false.
+            const currentScrollPos = window.scrollY;
+    
+            // Calculate opacity based on scroll position.
+            const opacity = Math.max(0, 1- currentScrollPos / scrollThreshPos+0.2);
+
+            setMenuOpacity(opacity);
+
+
+            if(currentScrollPos < scrollThreshPos) {
+                ref.current.style.position = 'fixed';
+            } else {
+                ref.current.style.position = 'relative';
+            }
+            
+        });
+
+        window.addEventListener('scroll', handleMenuPosition);
+        return () => window.removeEventListener('scroll', handleMenuPosition);
+
+    }, []);
+    
 
     return(
 
         <>
         <div className="preSection" id="preSectionMenu">
-            
+
             {/* Container for the menu bar */}
             <div id ='menuContainer' className='container'>
                 <div className='row justify-content-end'>
-                    <div id='menuBar' className='col-xl-3 col-lg-4 col-md-5 col-sm-12'>
+                    <div ref={ref} id='menuBar' className='col-xl-3 col-lg-4 col-md-5 col-sm-12'
+                    style={{
+                        opacity : menuOpacity,
+                        position : 'fixed',
+                        transition: 'all 0.6s ease', // Smooth transition for all property changes
+
+                    }}>
                         <MenuButtons/>
                     </div>
                 </div>
--- src/HomePage/firstSection/FirstSection.js
+++ src/HomePage/firstSection/FirstSection.js
@@ -2,38 +2,63 @@
 import TextContainer from '../textContainer/TextContainer'
 import MyIMG from '../myIMG/MyIMG'
 import './FirstSection.css'
-import { useRef } from 'react'
-import useScrollSlideAnimation from '../useScrollSlideAnimation'
+import { useRef, useEffect, useState } from 'react'
 
 
 function FirstSection() {
 
+    const [isVisible, setIsVisible] = useState(false);
     const ref = useRef(null);
-    const transformX = useScrollSlideAnimation(ref);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsVisible(entry.isIntersecting); // set isVisible to true;
+            },
+            {
+                root: null, // viewport
+            }
+        );
+
+        if(ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return (() => {
+            if (ref.current) {
+                observer.disconnect();
+            }
+        });
+
+    }, []);
+
+    
     return (
         <>
             {/* Container for the image and text */}
-            {/* <div ref={ref} className={`moving-element ${isVisible ? 'slide-in' : ''}`}> */}
-                <div id='textPicContainer' className='container' >
-                    <div ref={ref} id="textPicRow" className='row justify-content-start' style={{ transform: `translateX(${transformX}%)` }}>
+            <div id='textPicContainer' className='container' >
+                <div ref={ref} id="textPicRow" className='row justify-content-start'
+                        style={{transform: isVisible ? 'translateX(0)' : 'translateX(-100%)',
+                                opacity: isVisible ? 1 : 0,
+                                filter: isVisible ? 'blur(0px)' : 'blur(5px)',
+                                transition: 'transform 3s ease, opacity 4s ease, filter 4s ease'
+                                }}>
 
-                        <div id='myPic' className='col-lg-4 col-md-4 col-4'>
-                            <MyIMG/>
-                        </div>
-                        <div  id='textContainer' className='col-lg-8 col-md-8 col-8' >
+                    <div id='myPic' className='col-lg-4 col-md-4 col-4'>
+                        <MyIMG/>
+                    </div>
+                    <div  id='textContainer' className='col-lg-8 col-md-8 col-8' >
 
-                            <TextContainer/>
+                        <TextContainer/>
 
-                        </div>
-
-                        
                     </div>
 
+                    
                 </div>
-            {/* </div> */}
+
+            </div>
         </>
     )
-}
+};
 
-export default FirstSection
+export default FirstSection;
