9975c42f2412fc9077b98e390eb46131d491aa37
Brettler <liad.brettler@gmail.com>
2025-05-16 17:20:37 +0300

    Migrate to use Tailwind CSS V4.1.7 + fix broken ContactPage.js

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,8 +1,7 @@
-
 import './ContactPage.css'
-import { useState } from 'react';
+import {useState} from 'react';
 import MyInformation from './MyInformation/MyInformation';
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 // import {database} from '../../firebaseConfig'
@@ -11,24 +10,24 @@ import emailjs from '@emailjs/browser';
 function ContactPage() {
 
 
-/* 
-For flex-direction: row (the default):
+    /*
+    For flex-direction: row (the default):
 
-justify-content affects horizontal alignment (left-to-right in LTR languages).
-align-items affects vertical alignment (top-to-bottom).
-*/
+    justify-content affects horizontal alignment (left-to-right in LTR languages).
+    align-items affects vertical alignment (top-to-bottom).
+    */
 
-/*
-For flex-direction: column:
+    /*
+    For flex-direction: column:
 
-justify-content affects vertical alignment (top-to-bottom). --> justify-center 
-align-items affects horizontal alignment (left-to-right in LTR languages). ---> items-center
-Given this, under a flex-direction: column setup:
+    justify-content affects vertical alignment (top-to-bottom). --> justify-center
+    align-items affects horizontal alignment (left-to-right in LTR languages). ---> items-center
+    Given this, under a flex-direction: column setup:
 
-justify-content: center; will center the children vertically within the container (along the main axis, which is now vertical).
-align-items: center; will center the children horizontally within the container (along the cross axis, which is now horizontal).
+    justify-content: center; will center the children vertically within the container (along the main axis, which is now vertical).
+    align-items: center; will center the children horizontally within the container (along the cross axis, which is now horizontal).
 
-*/
+    */
 
     const [username, setUsername] = useState('');
     const [userEmail, setUserEmail] = useState('');
@@ -75,72 +74,79 @@ align-items: center; will center the children horizontally within the container 
 
 
     const sendEmail = (e) => {
-      e.preventDefault();
+        e.preventDefault();
 
-      const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-      const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-      const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
-      console.log(serviceID);
-      console.log(templateID);
-      console.log(publicKey);
+        const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+        const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+        const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+        console.log(serviceID);
+        console.log(templateID);
+        console.log(publicKey);
 
 
-      emailjs
-        .sendForm(serviceID, templateID, form.current, {
-          publicKey: publicKey,
-        })
-        .then(
-          () => {
-            setUsername('');
-            setUserEmail('');
-            setUserSubject('');
-            setUserMessage('');
-            setSuccessMessageSent('Your Message Sent Successfully, Will Be Touch Soon.')
-            setTimeout(() => setSuccessMessageSent(''), 5000)
-          },
-          (error) => {
-            setSuccessMessageSent(error.text)
-          },
-        );
+        emailjs
+            .sendForm(serviceID, templateID, form.current, {
+                publicKey: publicKey,
+            })
+            .then(
+                () => {
+                    setUsername('');
+                    setUserEmail('');
+                    setUserSubject('');
+                    setUserMessage('');
+                    setSuccessMessageSent('Your Message Sent Successfully, Will Be Touch Soon.')
+                    setTimeout(() => setSuccessMessageSent(''), 5000)
+                },
+                (error) => {
+                    setSuccessMessageSent(error.text)
+                },
+            );
     };
-
 
 
     return (
         <div className="contactPage relative flex flex-col items-center justify-center min-h-screen">
-            <div className='contactTitle absolute text-3xl sm:text-6xl top-0 sm:pt-12 pt-8 pb-8 text-white'>Contact Me!</div>
+            <div className='contactTitle absolute text-3xl sm:text-6xl top-0 sm:pt-12 pt-8 pb-8 text-white'>
+                Contact Me!
+            </div>
 
             <div className='formContainer container flex flex-col items-center justify-center sm:pb-4 pt-24'>
-                <form className='container grid w-5/6 sm:gap-16 gap-8 sm:m-0 sm:py-12 sm:pb-20 pr:32 py:6' 
+                <form
+                    className="grid w-full max-w-7xl sm:gap-16 gap-8 sm:m-0 sm:py-12 sm:pb-20 px-5 mx-auto"
                     onSubmit={sendEmail}
                     ref={form}
                     action='#'>
-                    <div className='grid sm:grid-cols-2 grid-cols-1 sm:gap-16 gap-8'>
-
+                    <div
+                        className='grid sm:grid-cols-2 grid-cols-1 sm:gap-16 gap-8'>
                         <MyInformation/>
 
-                        <input type='text' id='name' className='form-dotted-item h-10 w-full p-4  placeholder-white'
-                                required placeholder='Your Name' autoComplete='off' name="username" value={username}
-                               onChange={(e) => setUsername(e.target.value)}>
+                        <input
+                            type='text' id='name' className='form-dotted-item h-10 w-full p-4  placeholder-white'
+                            required placeholder='Your Name' autoComplete='off' name="username" value={username}
+                            onChange={(e) => setUsername(e.target.value)}>
                         </input>
 
-                        <input type='email' id='email' className='form-dotted-item h-10 w-full p-4  placeholder-white'
-                                required placeholder='Your Email Adress' autoComplete='off'  name="userEmail" value={userEmail}
-                               onChange={(e) => setUserEmail(e.target.value)}>
-
+                        <input
+                            type='email' id='email' className='form-dotted-item h-10 w-full p-4  placeholder-white'
+                            required placeholder='Your Email Adress' autoComplete='off' name="userEmail"
+                            value={userEmail}
+                            onChange={(e) => setUserEmail(e.target.value)}>
                         </input>
                     </div>
-                    
+
                     <div className='flex justify-center'>
-                        <input type='text' id='subject' className='form-dotted-item h-10 sm:w-1/2 w-full p-4 rounded-md  bg-slate-900 text-white placeholder-white'
-                                required placeholder='Subject' autoComplete='off' name="userSubject" value={userSubject}
-                               onChange={(e) => setUserSubject(e.target.value)}>
+                        <input
+                            type='text' id='subject'
+                            className='form-dotted-item h-10 sm:w-1/2 w-full p-4 rounded-md  bg-slate-900 text-white placeholder-white'
+                            required placeholder='Subject' autoComplete='off' name="userSubject" value={userSubject}
+                            onChange={(e) => setUserSubject(e.target.value)}>
                         </input>
                     </div>
 
                     <div className="flex justify-center">
-                        <textarea id='message' className='form-dotted-item text-wrap resize-none sm:w-1/2 w-full p-4 h-40 rounded-md  bg-slate-900 text-white placeholder-white'
-                                required placeholder="Message" name='userMessage' value={userMessage}
+                        <textarea id='message'
+                                  className='form-dotted-item text-wrap resize-none sm:w-1/2 w-full p-4 h-40 rounded-md  bg-slate-900 text-white placeholder-white'
+                                  required placeholder="Message" name='userMessage' value={userMessage}
                                   onChange={(e) => setUserMessage(e.target.value)}>
                         </textarea>
                     </div>
@@ -157,9 +163,7 @@ align-items: center; will center the children horizontally within the container 
                 </form>
             </div>
         </div>
-
     )
-
 }
 
 export default ContactPage;
--- src/Components/ContactPage/MyInformation/MyInformation.js
+++ src/Components/ContactPage/MyInformation/MyInformation.js
@@ -1,55 +1,39 @@
-
 import mailBoxIcon from '../iconsImg/icons8-email.png'
 import phoneIcon from '../iconsImg/icons8-phone.png'
 
 function MyInformation() {
-
-
-    return(
+    return (
         <>
-        
-        
-                                {/* Wrap the input and the icon in a div */}
-                        <div id='phoneContainer' className="form-dotted-item h-10 w-full flex items-center bg-sky-500 rounded-md py-4">
-                            {/* Render the icon */}
-                            <img src={phoneIcon} alt="Mailbox" id='myPhoneIcon' className="mailBoxIcon h-12 w-12 ml-2 mr-2" />
+            {/* Wrap the input and the icon in a div */}
+            <div id='phoneContainer'
+                 className="form-dotted-item h-10 w-full flex items-center bg-sky-500 rounded-md py-4">
+                {/* Render the icon */}
+                <img src={phoneIcon} alt="Mailbox" id='myPhoneIcon' className="mailBoxIcon h-12 w-12 ml-2 mr-2"/>
 
-                            {/* Adjust the input styles */}
-                            <span
-                                type='text'
-                                id='myPhone'
-                                className='flex-1 bg-transparent placeholder-white bg-sky-500'
-                                autoComplete='off'
+                {/* Adjust the input styles */}
+                <span
+                    id='myPhone'
+                    className='flex-1 bg-transparent placeholder-white bg-sky-500'
+                >
+                My Phone: (+972) 0547451234
+                </span>
+            </div>
 
-                            >
-                            My Phone: (+972) 0547451234
-                            </span>
-                        </div>
+            {/* Wrap the input and the icon in a div */}
+            <div id='mailContainer'
+                 className="form-dotted-item h-10 w-full flex items-center bg-sky-500 rounded-md py-4">
+                {/* Render the icon */}
+                <img src={mailBoxIcon} alt="Mailbox" id='myMailBoxIcon' className="mailBoxIcon h-12 w-12 ml-2 mr-2"/>
 
-
-                        {/* Wrap the input and the icon in a div */}
-                        <div id='mailContainer' className="form-dotted-item h-10 w-full flex items-center bg-sky-500 rounded-md py-4">
-                            {/* Render the icon */}
-                            <img src={mailBoxIcon} alt="Mailbox" id='myMailBoxIcon' className="mailBoxIcon h-12 w-12 ml-2 mr-2" />
-
-                            {/* Adjust the input styles */}
-                            <span
-                                type='text'
-                                id='myEmail'
-                                className='flex-1 bg-transparent placeholder-white bg-sky-500'
-                                autoComplete='off'
-
-                            >
-                            E-Mail: liad.brettler@gmail.com
-                            </span>
-                        </div>
-        
-        
-        
-        
+                {/* Adjust the input styles */}
+                <span
+                    id='myEmail'
+                    className='flex-1 bg-transparent placeholder-white bg-sky-500'
+                >
+                E-Mail: liad.brettler@gmail.com
+                </span>
+            </div>
         </>
-
-
     )
 }
 
